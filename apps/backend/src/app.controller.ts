e040657508d83725f9f934b580a386770b20fe5a
Marek Fodor <fodormarek5@gmail.com>
2025-06-04 11:30:21 +0200

    chore(backend): Provide example for controller and routes.

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,44 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AbilityProvider } from '@authorization/authorization.provider';
+import { Public } from '@authentication/decorators/public.decorator';
+import { Optional } from '@authentication/decorators/optional.decorator';
+import { Action } from '@authorization/constants/casl.constants';
+import { AppAbility } from '@authorization/types/authorization.types';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // Ability can be null in case of public route.
+  private ability: AppAbility; // EXAMPLE - Remove this line if not needed
 
+  constructor(
+    private readonly appService: AppService,
+    private readonly abilityProvider: AbilityProvider, // EXAMPLE - Remove this line if not needed
+  ) {
+    this.ability = this.abilityProvider.getAbility(); // EXAMPLE - Remove this line if not needed
+  }
+
+  // EXAMPLE - Remove this route if not needed
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getExampleProtected(): string {
+    return 'This is a protected route example - you should not see this if you are not authenticated.';
+  }
+
+  // EXAMPLE - Remove this route if not needed
+  @Get('public')
+  @Public()
+  getExamplePublic(): string {
+    return 'This is a public route example.';
+  }
+
+  // EXAMPLE - Remove this route if not needed - User read permission is needed!
+  @Get('optional')
+  @Optional()
+  getExampleOptional(): string {
+    if (this.ability && this.ability.can(Action.Read, 'user')) {
+      return 'This is an optional route example with permitted user.';
+    }
+
+    return 'This is an optional route example without (permitted) user.';
   }
 }
